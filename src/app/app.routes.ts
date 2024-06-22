42116b4cc1b8e1f379b4d7e059acfb244d6de55e
DivyaDarshanTiwari <davidrocco400@outlook.com>
2024-06-22 23:47:43 +0530

    removed unneccesary import from router.ts file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
-import { HomePageComponent } from './home/home-page/home-page.component';
-import { NewComponent } from './Content Page/NewComponent/new/new.component';
 
 export const routes: Routes = [
     {path:'',
