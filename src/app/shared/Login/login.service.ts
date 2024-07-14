963334ac6cb59b6acdcc7be9720687aadd0c501e
DivyaDarshanTiwari <davidrocco400@outlook.com>
2024-07-14 22:40:51 +0530

    added data to local storage

--- src/app/shared/Login/login.service.ts
+++ src/app/shared/Login/login.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { catchError, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -10,30 +10,27 @@ export class LoginService {
   private url = 'http://localhost:3000/user_data';
   isLoggedIn = false;
 
-  constructor(private http: HttpClient, private route:Router) {}
+  constructor(private http: HttpClient, private route: Router) {
+    let local = localStorage.getItem('Previous_Logged')
+    if(local){
+      this.isLoggedIn = JSON.parse(local);
+    }
+  }
 
   public getData(): Observable<any> | null {
     if (this.url) {
-      return this.http.get(this.url);
+      return this.http.get(this.url).pipe(
+        catchError((error) => {
+          return throwError(error);
+        })
+      );
     } else {
       console.error('Url not found');
       return null;
     }
   }
 
-  public submit(user_name: string, password: string , email:string): void {
-    if (user_name && password && email) {
-      console.log(user_name, password, email);
-      this.http.post(this.url, { user: user_name, password: password , email:email})
-        .subscribe((response) => {
-          console.log(response);
-          this.isLoggedIn= true;
-          this.route.navigateByUrl('profile');
-        }, (error) => {
-          console.error(error);
-        });
-    } else {
-      alert("Enter both the details");
-    }
+  public save_local(){
+    localStorage.setItem('Previous_Logged', JSON.stringify(this.isLoggedIn));
   }
 }
