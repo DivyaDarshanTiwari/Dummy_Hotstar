323a9d3e72f6810ae9a64ff0f7477dbdfdfd16d1
DivyaDarshanTiwari <davidrocco400@outlook.com>
2024-07-14 22:47:34 +0530

    added condition ture return true or not

--- src/app/Authrntication/Login/login.guard.ts
+++ src/app/Authrntication/Login/login.guard.ts
@@ -1,9 +1,12 @@
 import { CanActivateFn } from '@angular/router';
 import { LoginService } from '../../shared/Login/login.service';
 import { inject } from '@angular/core';
+import { ActivatedRouteSnapshot,RouterStateSnapshot,Router } from '@angular/router';
 
 
-export const loginGuard: CanActivateFn = (route, state) => {
+export const loginGuard: CanActivateFn = (route:ActivatedRouteSnapshot, state:RouterStateSnapshot) => {
   let isLogged = inject(LoginService)
-  return isLogged.isLoggedIn;
+  let router = inject(Router);
+  const protected_url : string[] = ["/profile"]
+  return protected_url.includes(state.url) && isLogged.isLoggedIn ? true : router.navigateByUrl('');
 };
