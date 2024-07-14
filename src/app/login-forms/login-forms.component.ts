425b0958e353a066ebc74b91629cd67ab0a6ef18
DivyaDarshanTiwari <davidrocco400@outlook.com>
2024-07-14 22:48:28 +0530

    added logic if alowed to login or not

--- src/app/login-forms/login-forms.component.ts
+++ src/app/login-forms/login-forms.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { LoginService } from '../shared/Login/login.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login-forms',
   standalone: true,
@@ -9,28 +10,33 @@ import { LoginService } from '../shared/Login/login.service';
   styleUrl: './login-forms.component.css',
 })
 export class LoginFormsComponent implements OnInit {
-  constructor(private login_service: LoginService ) {}
+  constructor(private login_service: LoginService, private route: Router) {}
   private data: any;
+  private password!: string;
   ngOnInit(): void {
     this.getdata();
   }
   applyform = new FormGroup({
     user_name: new FormControl(''),
     password: new FormControl(''),
-    email:new FormControl('')
+    email: new FormControl(''),
   });
 
   getdata() {
-    this.login_service.getData()?.subscribe((next)=>{
+    this.login_service.getData()?.subscribe((next) => {
       this.data = next;
       console.log(this.data);
-    })
+    });
   }
-  submitApplication(){
-    this.login_service.submit(
-      this.applyform.value.user_name ?? '',
-      this.applyform.value.password ?? '',
-      this.applyform.value.email ?? ''
-    )
+
+  submitApplication() {
+    this.login_service.getData();
+    this.password = this.applyform.value.password ?? '';
+    let find = this.data.find((value: any) => value.password == this.password);
+    if (find) {
+      this.login_service.isLoggedIn = true;
+      this.login_service.save_local();
+      this.route.navigateByUrl('profile');
+    }
   }
 }
