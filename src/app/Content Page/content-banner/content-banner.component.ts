600d2b6acb8d616625cd17144edeaa6f32e2158f
DivyaDarshanTiwari <davidrocco400@outlook.com>
2024-07-17 00:55:05 +0530

    checking user and the wishList if it belocng to it

--- src/app/Content Page/content-banner/content-banner.component.ts
+++ src/app/Content Page/content-banner/content-banner.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 import { Data1Interface } from '../../data1-interface';
 import { SelectedhCardInterface } from '../../shared/search/selectedh-card-interface';
 import { CharaterInterface } from '../../shared/Characters/charater-interface';
+import { WatchListService } from '../../shared/WatchList/watch-list.service';
 
 @Component({
   selector: 'app-content-banner',
@@ -15,24 +16,38 @@ import { CharaterInterface } from '../../shared/Characters/charater-interface';
 })
 export class ContentBannerComponent implements OnInit {
 
+  constructor(private watchList_Service:WatchListService){
+  }
+
   // data comming from the new component
   @Input({required: true}) item?: Data1Interface | null;
   @Input({required: true}) selected_card_data?:SelectedhCardInterface | null;
   @Input({required: true}) selected_character?: CharaterInterface | null;
+   private selected_user_id?: number;
 
   protected add_to_watchList: boolean = false;
 
 
   ngOnInit(): void {
+    this.selected_user_id = this.watchList_Service.get_Selected_id();
+    let x=[]
+    x = this.watchList_Service.getSecond_WatchList().filter((value)=> value.item.mal_id == this.selected_card_data?.mal_id)
+    x = x.filter((value)=> this.selected_user_id == value.user_id)
+    if(x.length){
+    this.add_to_watchList = true;
+      console.log(this.add_to_watchList);
+    }
   }
   // item = data1[1];
 
-  watchList() {
+  watchList(location: string, item: any) {
     if(this.add_to_watchList === true){
       this.add_to_watchList = false
+      this.watchList_Service.set_watch_list(location,item,this.add_to_watchList)
     }
     else{
       this.add_to_watchList = true
+      this.watchList_Service.set_watch_list(location,item,this.add_to_watchList)
     }
     }
 
