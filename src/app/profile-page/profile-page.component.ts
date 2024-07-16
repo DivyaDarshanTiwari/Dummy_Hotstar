195039ff597f854d45d46474557b3bc420d7e354
DivyaDarshanTiwari <davidrocco400@outlook.com>
2024-07-17 00:55:40 +0530

    nothing

--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -25,7 +25,7 @@ import { WatchListService } from '../shared/WatchList/watch-list.service';
 })
 export class ProfilePageComponent implements OnInit {
   constructor(
-    private userData: UserPDataService,//instace of the personal data servise
+    private userData: UserPDataService, //instace of the personal data servise
     private watchData: WatchListService //instance of the watch list data service
   ) {}
   protected phone!: number;
@@ -34,7 +34,7 @@ export class ProfilePageComponent implements OnInit {
   protected DOB!: string;
   protected id!: number;
   protected img!: string;
-  protected Previous_ActiveID!:number;
+  protected Previous_ActiveID!: number;
   ngOnInit(): void {
     // initializing the data with the active userData that was previously selected when the browser was closed
     this.phone = this.userData.get_Actived_data()!.phone;
@@ -43,14 +43,14 @@ export class ProfilePageComponent implements OnInit {
     this.DOB = this.userData.get_Actived_data()!.DOB;
     this.id = this.userData.get_Actived_data()!.userId;
     this.img = this.userData.get_Actived_data()!.profile_img;
-    this.Previous_ActiveID = this.userData.get_Actived_data()!.userId
+    this.Previous_ActiveID = this.userData.get_Actived_data()!.userId;
   }
 
   protected user_Personal_Data: UserPData[] = this.userData.getPorfileData();
 
   protected data1: WatchListInterface[] = this.watchData.getWatchList();
 
-  protected second_watchList: any[] = this.watchData.getSecond_WatchList()
+  protected second_watchList: any[] = this.watchData.getSecond_WatchList();
 
   protected returned_Data(data: UserPData) {
     this.phone = data.phone;
@@ -59,9 +59,8 @@ export class ProfilePageComponent implements OnInit {
     this.DOB = data.DOB;
     this.id = data.userId;
     this.img = data.profile_img;
-    this.userData.set_Deactive_data(this.Previous_ActiveID);// the previous key is deactivated here
-    this.Previous_ActiveID = data.userId;// the new user profile that is being selected is the set as previous data for the next change of the profile
-    this.watchData.set_Selected_id(this.id)// sending id of the selected data to the watchlist service
+    this.userData.set_Deactive_data(this.Previous_ActiveID); // the previous key is deactivated here
+    this.Previous_ActiveID = data.userId; // the new user profile that is being selected is the set as previous data for the next change of the profile
+    this.watchData.set_Selected_id(this.id); // sending id of the selected data to the watchlist service
   }
-
 }
