5df9085f1e63463cca1d90ce4b84c57202728ef8
DivyaDarshanTiwari <davidrocco400@outlook.com>
2024-07-18 14:24:44 +0530

    logig of when to nagivage to login

--- src/app/Content Page/content-banner/content-banner.component.ts
+++ src/app/Content Page/content-banner/content-banner.component.ts
@@ -6,16 +6,22 @@ import { Data1Interface } from '../../data1-interface';
 import { SelectedhCardInterface } from '../../shared/search/selectedh-card-interface';
 import { CharaterInterface } from '../../shared/Characters/charater-interface';
 import { WatchListService } from '../../shared/WatchList/watch-list.service';
+import { LoginService } from '../../shared/Login/login.service';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-content-banner',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, CommonModule],
+  imports: [MatButtonModule, MatIconModule, CommonModule, RouterLink],
   templateUrl: './content-banner.component.html',
   styleUrls: ['./content-banner.component.css'],
 })
 export class ContentBannerComponent implements OnInit {
-  constructor(private watchList_Service: WatchListService) {}
+  constructor(
+    private watchList_Service: WatchListService,
+    private loginService: LoginService,
+    private router: Router
+  ) {}
 
   // data comming from the new component
   @Input({ required: true }) item?: Data1Interface | null;
@@ -26,60 +32,80 @@ export class ContentBannerComponent implements OnInit {
   protected add_to_watchList: boolean = false;
 
   ngOnInit(): void {
-    this.selected_user_id = this.watchList_Service.get_Selected_id();
-    let x = this.watchList_Service.getSecond_WatchList();
-    let z = this.watchList_Service.getSecond_WatchList();
-    /**
-     * @description seeing the initial loading if dat is in wishlist
-     */
-    z = z.filter((value)=>{
-      return value.loaction == 'api' && value.item_id == this.selected_card_data?.mal_id && this.selected_user_id == value.user_id
-    })
-    if(!(z.length)){
+    if (this.loginService.isLogged()) {
+      this.selected_user_id = this.watchList_Service.get_Selected_id();
       let x = this.watchList_Service.getSecond_WatchList();
-      x = x.filter((value)=>{
-        return value.loaction == 'local' && value.item_id == this.item?.id && this.selected_user_id == value.user_id
-      })
+      let z = this.watchList_Service.getSecond_WatchList();
+      /**
+       * @description seeing the initial loading if dat is in wishlist
+       */
+      z = z.filter((value) => {
+        return (
+          value.loaction == 'api' &&
+          value.item_id == this.selected_card_data?.mal_id &&
+          this.selected_user_id == value.user_id
+        );
+      });
+      if (!z.length) {
+        let x = this.watchList_Service.getSecond_WatchList();
+        x = x.filter((value) => {
+          return (
+            value.loaction == 'local' &&
+            value.item_id == this.item?.id &&
+            this.selected_user_id == value.user_id
+          );
+        });
+      }
+      if (z.length || x.length) {
+        this.add_to_watchList = true;
+        console.log(this.add_to_watchList);
+      }
     }
-    if (z.length || x.length) {
-      this.add_to_watchList = true;
-      console.log(this.add_to_watchList);
+  }
+
+  logIn() {
+    if (!this.loginService.isLogged()) {
+      this.router.navigateByUrl('login');
+    } else {
+      console.log('logged in previously');
     }
   }
 
   watchList(location: string, item: any) {
-    if (this.add_to_watchList === true) {
-      this.add_to_watchList = false;
-      /**
-       * @description condition used to identify the way item id will be accessed sent 
-       */
-      if (location == 'api') {
-        this.watchList_Service.set_watch_list(
-          location,
-          item.mal_id,
-          this.add_to_watchList
-        );
+    if (this.loginService.isLogged()) {
+      if (this.add_to_watchList === true) {
+        this.add_to_watchList = false;
+        /**
+         * @description condition used to identify the way item id will be accessed sent
+         */
+        if (location == 'api') {
+          this.watchList_Service.set_watch_list(
+            location,
+            item.mal_id,
+            this.add_to_watchList
+          );
+        } else {
+          this.watchList_Service.set_watch_list(
+            location,
+            item.id,
+            this.add_to_watchList
+          );
+        }
       } else {
-        this.watchList_Service.set_watch_list(
-          location,
-          item.id,
-          this.add_to_watchList
-        );
-      }
-    } else {
-      this.add_to_watchList = true;
-      if (location == 'api') {
-        this.watchList_Service.set_watch_list(
-          location,
-          item.mal_id,
-          this.add_to_watchList
-        );
-      } else {
-        this.watchList_Service.set_watch_list(
-          location,
-          item.id,
-          this.add_to_watchList
-        );
+        this.add_to_watchList = true;
+        if (location == 'api') {
+          this.watchList_Service.set_watch_list(
+            location,
+            item.mal_id,
+            this.add_to_watchList
+          );
+        } else {
+          this.watchList_Service.set_watch_list(
+            location,
+            item.id,
+            this.add_to_watchList
+          );
+        }
       }
     }
   }
